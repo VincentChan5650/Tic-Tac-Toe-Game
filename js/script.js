cf952da07e204bec1430ee67ad8e91d2d2e8d217
VincentChan5650 <vin9305@gmail.com>
2017-09-20 13:59:50 -0700

    first commit
    create a tic tac toe game with jQuery

---
+++ js/script.js
@@ -0,0 +1,120 @@
+//check if the document is ready
+$(document).ready(function(){
+  var x = "x";
+  var o = "o";
+  var turns = 0;
+
+  var spot1 = $('#spot1');
+  var spot2 = $('#spot2');
+  var spot3 = $('#spot3');
+  var spot4 = $('#spot4');
+  var spot5 = $('#spot5');
+  var spot6 = $('#spot6');
+  var spot7 = $('#spot7');
+  var spot8 = $('#spot8');
+  var spot9 = $('#spot9');
+
+
+  //get the li element with a click event
+  $('#board li').on('click', function(){
+    //listening to all the winning conditions
+    if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+      spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+      spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+      spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+      spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+      spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+      spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+      spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')
+  ){
+
+    alert("Winner: o");
+    //resetting the game
+    $('#board li').text('+');
+    $('#board li').removeClass('disable');
+    $('#board li').removeClass('o');
+    $('#board li').removeClass('x');
+
+
+  }else if(spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
+    spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
+    spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
+    spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
+    spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
+    spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
+    spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
+    spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')){
+
+      alert("Winner: x");
+      //resetting the game
+      $('#board li').text('+');
+      $('#board li').removeClass('disable');
+      $('#board li').removeClass('o');
+      $('#board li').removeClass('x');
+
+    }else if(turns == 9){
+      // if all spot filled but no winner, alert 'tie game'
+
+      alert("Tie Game");
+      //resetting the game
+      $('#board li').text('+');
+      $('#board li').removeClass('disable');
+      $('#board li').removeClass('o');
+      $('#board li').removeClass('x');
+      turns = 0;
+
+
+    }else if($(this).hasClass('disable')){
+      // if the spot has diable class, meaning it is filled
+      alert('This spot is filled');
+
+    }else if(turns%2 == 0){
+      // 'o' goes first, then 'x'
+      turns++; //increment of turns for keeping track
+      $(this).text(o); //mark the spot with o
+      $(this).addClass('disable o'); //when click on spot, add two classes: disable and o
+
+      if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+        spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+        spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+        spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+        spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+        spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+        spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+        spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')
+    ){
+      alert('Winner: o');
+      turns = 0;
+    }
+
+
+  } else {
+      turns++;
+      $(this).text(x);
+      $(this).addClass('disable x'); //when click on spot, add two classes: disable and x
+      //listening to the winning conditions
+      if(spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
+        spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
+        spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
+        spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
+        spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
+        spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
+        spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
+        spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')
+    ){
+      alert('Winner: x');
+      //reset the turns if game is ended
+      turns = 0;
+    }
+    }
+  });
+
+  // set the functions of reset button
+  $('#reset').on('click', function(){
+    $('#board li').text('+');
+    $('#board li').removeClass('disable');
+    $('#board li').removeClass('o');
+    $('#board li').removeClass('x');
+    turns = 0;
+  });
+});
